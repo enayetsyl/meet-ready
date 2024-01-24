f6b944ccd372bbe011329b0df1aaa2a2dd01e1b6
Ashfakul Islam <r4pido3@gmail.com>
2024-01-24 23:14:03 +0600

    [fix/dashboard] (dashboard navbar responsive) : dashboard navbar is responsived now

--- src/app/(dashboard)/dashboard/scheduled-event/page.js
+++ src/app/(dashboard)/dashboard/scheduled-event/page.js
@@ -3,7 +3,7 @@ import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const page = () => {
   return (
-    <div className="flex flex-col justify-start pt-10 space-y-5">
+    <div className="flex flex-col justify-start mt-16 space-y-5">
       <h1 className="text-2xl font-semibold pl-2 border-l-2 border-purple-500">
         Scheduled Events
       </h1>
