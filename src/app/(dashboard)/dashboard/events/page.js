fb11d4c205919282741cf5cd789e796d192f7130
Enayet <enayetflweb@gamil.com>
2024-01-25 18:05:10 +0600

    event and scheduledEvent page name update

--- src/app/(dashboard)/dashboard/events/page.js
+++ src/app/(dashboard)/dashboard/events/page.js
@@ -2,7 +2,7 @@
 import EventCard from "@/components/EventCard/EventCard";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
-const page = () => {
+const Events = () => {
   return (
     <div className="flex flex-col justify-start mt-16 space-y-5">
       <div className="flex justify-center items-center relative border-2">
@@ -31,4 +31,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Events;
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,7 @@
 import mongoose from 'mongoose'
 
 const connect = async () => {
-  // if(mongoose.connection[0].readyState) return;
-
-  try {
+    try {
     await mongoose.connect(process.env.MONGO_URL)
       console.log("Mongo connection successfully established.")
   } catch(error){
